537b0f2497cef73c7472c3142d9e1f503a2120b4
Camperoh7 <gibran_h7@hotmail.com>
2022-10-25 13:46:29 -0500

    🐛 logo y nombre

--- menu-chinicuil/src/pages/Menu.jsx
+++ menu-chinicuil/src/pages/Menu.jsx
@@ -9,6 +9,7 @@ const Menu = () => {
         <div className='menu'>
         <div className='encabezado parte_izquierda'>
             <h4>Desayunos</h4>
+            <p>- Desayuno Completo <strong>$180</strong>. (Incluye yogurth con cereal, jugo de temporada, café de olla y huevos al gusto ó chilaquiles ó flautas de pollo/papa)</p>
             <p>- Omelette de Espinaca con Queso de Cabra <strong>$150</strong></p>
             <p>- Omelette de Hongos con Quesillo <strong>$150</strong></p>
             <p>- Omelette de Huitlacoche <strong>$150</strong></p>
@@ -26,23 +27,63 @@ const Menu = () => {
             <p>- Flautas de Barbacoa (Orden de 3pz) <strong>$150</strong></p>
             <p>- Quesadilla de Barbacoa <strong>$140</strong></p>
             <p>- Consome con carne <strong>90</strong></p>
-            <p>- Consome sin carne <strong>50</strong></p>
+            <p>- Consome sin carne <strong>50</strong></p><hr/>
+            <h4>Bebidas</h4>
+            <p>- Agua del Día <strong>$45</strong></p>
+            <p>- Boing <strong>$45</strong></p>
+            <p>- Jugo Verde <strong>$90</strong></p>
+            <p>- Coca Cola <strong>$45</strong></p><hr/>
+            <h4>Cervezas</h4>
+            <p>- Corona <strong>$50</strong></p>
+            <p>- Pacífico <strong>$50</strong></p>
+            <p>- Montejo <strong>$50</strong></p>
+            <p>- Negra Modelo <strong>$60</strong></p>
+            <p>- Modelo Especial <strong>$60</strong></p>
         </div>
         <div className='encabezado parte_derecha'>
+            <h4>Entradas</h4>
+            <p>- Guacamole <strong>$220</strong></p>
+            <p>- Nachos con Carne <strong>$280</strong></p><hr/>
             <h4>Platillos</h4>
-            <p>- Molcajete Mar y Tierra <strong>$720</strong></p>
+            <p>- Molcajete Mar y Tierra. (Frijoles rancheros, arrachera, camarones, queso hecho por chinicuil, chorizo, nopales, cebollines, chile serrano y tortillas hechas a mano 6pz) Extra de tortillas 6pz <strong>+$25.  </strong> <strong>$720</strong></p>
+            <p>- Pollo Entero Estilo Hidalgo. (En penca de Maguey con adobo y nopales) <strong>$450</strong></p>
+            <p>- Pollo con Mole Poblano o Mole Verde <strong>$240</strong></p>
+            <p>- Pollo Asado Chinicuil <strong>$220</strong></p>
+            <p>- Camarones al Axiote <strong>$350</strong></p>
+            <p>- Camarones al Ajillo <strong>$350</strong></p>
+            <p>- Arrachera Chinicuil <strong>$350</strong></p>
             <p>- Tlacoyos (Orden de 3pz) <strong>$60</strong>. Con pollo <strong>+$50</strong>, con cecina <strong>+$60</strong>, con arrachera <strong>+$80</strong></p>
             <p>- Sopes (Orden de 3pz) <strong>$90</strong>. Con pollo <strong>+$50</strong>, con cecina <strong>+$60</strong>, con arrachera <strong>+$80</strong></p>
             <p>- Taco de Cecina <strong>$45</strong></p>
             <p>- Taco de Arrachera <strong>$65</strong></p>
             <p>- Caldo de Pollo <strong>$100</strong></p>
-            <p>- Sopa de Tortilla <strong>$130</strong></p><hr/>
+            <p>- Sopa de Tortilla <strong>$130</strong></p>
+            <p>- Sopa del Día <strong>$130</strong></p><hr/>
             <h4>Vegano</h4>
             <p>- Quesadilla de Hongos <strong>$110</strong></p>
             <p>- Quesadilla de Huitlacoche <strong>$130</strong></p>
             <p>- Tostadas de Flor de Jamaica <strong>$120</strong></p>
-            <p>- Flautas de Tinga de Zanahoria <strong>$150</strong></p><hr/>
-            <h4>Bebidas</h4>
+            <p>- Flautas de Papa <strong>$120</strong></p><hr/>
+            <h4>Tequila</h4>
+            <p>- Don Julio reposado <strong>$200</strong></p>
+            <p>- Mestro Dobel <strong>$220</strong></p>
+            <p>- Herradura Ultra <strong>$250</strong></p>
+            <p>- Patrón Silver <strong>$260</strong></p>
+            <p>- José Cuervo Especial <strong>$180</strong></p><hr/>
+            <h4>Mezcal</h4>
+            <p>- La Kuka Espadín <strong>$</strong></p>
+            <h4>Ginebra</h4>
+            <p>- Tanqueray Ten <strong>$250</strong></p>
+            <p>- Bombay <strong>$250</strong></p><hr/>
+            <h4>Whisky</h4>
+            <p>- Buchanan´s 12 <strong>$200</strong></p>
+            <p>- Macallan 12 <strong>$300</strong></p><hr/>
+            <h4>Cocteles</h4>
+            <p>- MARGARITA <strong>$150</strong></p>
+            <p>- MEZCALITA <strong>$220</strong></p>
+            <p>- MOJITO <strong>$150</strong></p>
+            <p>- CANTARITO <strong>$150</strong></p>
+
         </div>
         </div>
         </>
